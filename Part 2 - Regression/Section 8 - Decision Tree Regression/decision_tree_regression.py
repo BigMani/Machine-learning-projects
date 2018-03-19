c1bb78ba0a9ed015aad909ed7b14eefce36a5f97
Arun Nemani <arunnemani@Aruns-MacBook-Pro.local>
2018-03-19 15:11:23 -0500

    Added Random forest examples

--- Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.py
+++ Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.py
@@ -1,4 +1,5 @@
 # Decision Tree Regression
+
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,11 +8,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
-y = dataset.iloc[:, -1].values
-
-# Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+Y = dataset.iloc[:, -1].values
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -21,8 +18,10 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting the Regression Model to the dataset
-# Create your regressor here
+# Fitting the Decision tree regression model to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X, Y)
 
 # Calculate R^2 and adjusted R^2
 def calculateRSquared(y_orig, y_pred, X):
@@ -32,23 +31,11 @@ def calculateRSquared(y_orig, y_pred, X):
     R_squared_adjusted = 1 - (1-R_squared)*(len(y_orig)-1)/(len(y_orig)-X.shape[1]-1)
     return R_squared, R_squared_adjusted
 
-# Predicting a new result
-y_pred = regressor.predict(6.5)
-
-# Visualising the Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Regression Model)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
-
-# Visualising the Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.1)
+# Visualising the decision tree results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
+plt.scatter(X, Y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Regression Model)')
+plt.title('Truth or Bluff (Decision tree regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
-plt.show()
