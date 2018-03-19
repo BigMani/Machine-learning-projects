1029e642593e81da4798f3bd80f4ba2497b053f1
Arun Nemani <arunnemani@Aruns-MacBook-Pro.local>
2018-03-19 17:51:11 -0500

    added random forest regression

--- Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.py
+++ Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.py
@@ -7,12 +7,8 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
-
-# Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+X = dataset.iloc[:, 1:-1].values
+Y = dataset.iloc[:, -1].values
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -22,20 +18,27 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Random Forest Regression to the dataset
+# Fitting the Random forest regression model to the dataset
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
-regressor.fit(X, y)
+regressor = RandomForestRegressor(n_estimators=100,random_state=0)
+regressor.fit(X, Y)
+
+# Calculate R^2 and adjusted R^2
+def calculateRSquared(y_orig, y_pred, X):
+    Resid = sum((y_orig - y_pred)**2)
+    Total = sum((y_orig - np.mean(y_orig))**2)
+    R_squared = 1 - (float(Resid))/Total
+    R_squared_adjusted = 1 - (1-R_squared)*(len(y_orig)-1)/(len(y_orig)-X.shape[1]-1)
+    return R_squared, R_squared_adjusted
 
 # Predicting a new result
-y_pred = regressor.predict(6.5)
-
-# Visualising the Random Forest Regression results (higher resolution)
+Y_pred = regressor.predict(6.5)
+# Visualizing the random forest tree results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
+plt.scatter(X, Y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Random Forest Regression)')
+plt.title('Truth or Bluff (Random forest regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
